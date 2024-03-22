639f89849e6b59d30dbcde4a14667e9a1431bc70
Sela van Vreeswijk <102545946+selavanvreeswijk@users.noreply.github.com>
2024-03-22 19:04:53 +0100

    code efficiënter gemaakt & nagelopen

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// vóór begin spel
+// beginwaarden ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 const zwarteAchtergrond = document.querySelector('.zwarteachtergrond')
 const titelSpelen = document.querySelector('header div')
 const paginaSpelen = document.querySelector('.spelenpagina')
 const confetti = document.querySelector('.confetti')
 const spelLaden = document.querySelector('.spelladen')
-const klikOpKnop = new Audio ('./audio/buttonpress.mp3') // bron https://pixabay.com/sound-effects/notification-for-game-scenes-132473/ 
-const gameLadenGeluid = new Audio ('./audio/lasercharging.mp3') // https://pixabay.com/sound-effects/062708-laser-charging-81968/
-const spelerGekozenGeluid = new Audio ('./audio/teleport.mp3') // https://pixabay.com/sound-effects/teleport-90137/
+const klikOpKnop = new Audio ('./audio/buttonpress.mp3') // bron geluid https://pixabay.com/sound-effects/notification-for-game-scenes-132473/ 
+const gameLadenGeluid = new Audio ('./audio/lasercharging.mp3') // bron geluid https://pixabay.com/sound-effects/062708-laser-charging-81968/
+const spelerGekozenGeluid = new Audio ('./audio/teleport.mp3') // bron geluid https://pixabay.com/sound-effects/teleport-90137/
 const spookyMuziekAfspelen = document.querySelector('header p') 
-const spookyMuziekGeluid = new Audio ('./audio/scarymusic.mp3') // / bron https://pixabay.com/sound-effects/scary-music-box-for-spooky-scenes-165983/
+const spookyMuziekGeluid = new Audio ('./audio/scarymusic.mp3') // / bron geluid https://pixabay.com/sound-effects/scary-music-box-for-spooky-scenes-165983/
 let muziekStarten = true;
 
 zwarteAchtergrond.style.display = 'none';
@@ -18,29 +18,33 @@ paginaSpelen.style.display = 'none';
 confetti.style.display = 'none';
 spelLaden.style.display = 'none';
 
-// ------------------------------naam invoeren--------------------------------
+// ------------------------------naam invoeren-------------------------------------------------------------------------------------------------------------------------
 
 // invulveld naam invoeren omhoog
 
 const gamenaamTekst = document.querySelector('.gamenaaminvoeren')
 const titelIndex = document.querySelector('header section')
 const startKnop = document.querySelector('header section button')
-const audioRockIntro = new Audio ('./audio/rockintro.mp3') // bron https://pixabay.com/sound-effects/yeah-18130/
+const audioRockIntro = new Audio ('./audio/rockintro.mp3') // bron geluid https://pixabay.com/sound-effects/yeah-18130/
 
 function opKnopKlikken(){
     klikOpKnop.play()
     klikOpKnop.volume = 0.05; // bron https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volume
 }
 
+// knop enge muziek afspelen beginpagina
+
 spookyMuziekAfspelen.addEventListener('click', () => {
     if (muziekStarten){
+        muziekStarten = false;
+
         spookyMuziekGeluid.play()
         spookyMuziekGeluid.volume = 0.2;
-        muziekStarten = false;
         spookyMuziekAfspelen.style.backgroundColor = 'rgb(1,237,143)';
     } else{
-        spookyMuziekGeluid.pause()
         muziekStarten = true;
+        
+        spookyMuziekGeluid.pause()
         spookyMuziekAfspelen.style.backgroundColor = 'rgb(233,51,153)';
     } 
 })
@@ -50,15 +54,15 @@ startKnop.addEventListener('click', () => {
     audioRockIntro.play()
     audioRockIntro.volume = 0.3;
     spookyMuziekGeluid.pause();
-    
+
     gamenaamTekst.style.display = 'block';
     titelIndex.style.display = 'none';
     zwarteAchtergrond.style.display = 'block';
 })
 
-// ------------------------------naam invoeren--------------------------------
+// ------------------------------naam invoeren-------------------------------------------------------------------------------------------------------------------------
 
-// -----------------------naam invoeren + grap------------------------
+// -----------------------naam invoeren + grap-----------------------------------------------------------------------------------------------------------------
 
 const naamGebruiker = document.querySelector('input')
 const berichtVerschijnen = document.querySelector('.gamenaaminvoeren p')
@@ -91,9 +95,9 @@ knopIndienen.addEventListener("click", () => {
     }, 1500);
 })
 
-// -----------------------naam invoeren + grap------------------------
+// -----------------------naam invoeren + grap-------------------------------------------------------------------------------------------------------
 
-// ------------------------character kiezen---------------------------
+// ------------------------speler kiezen--------------------------------------------------------------------------------------------------------------------
 
 const characterKiezen = document.querySelector('.characterkiezen')
 const characterLevi = document.getElementById('levi') // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
@@ -101,13 +105,13 @@ const characterEren = document.getElementById('eren')
 const characterHange = document.getElementById('hange')
 const gekozenCharacter = document.getElementById('charactergebruiker')
 
-characterKiezen.style.display ='none';
+characterKiezen.style.display = 'none';
 
 volgendeKnop.addEventListener("click", () => {
     opKnopKlikken()
-    titelIndex.style.display = "none";
-    gamenaamTekst.style.display = "none";
-    volgendeKnop.style.display ='none';
+    titelIndex.style.display = 'none';
+    gamenaamTekst.style.display = 'none';
+    volgendeKnop.style.display = 'none';
     characterKiezen.style.display ='block';
 })
 
@@ -116,24 +120,22 @@ volgendeKnop.addEventListener("click", () => {
 const alleSpelers = [characterEren, characterHange, characterLevi];
 
 alleSpelers.forEach(speler => {
-    speler.addEventListener('click', () => { // <-- Hier is de komma verwijderd
-        gameLaden();
+    speler.addEventListener('click', () => {
         gekozenCharacter.src = `./images/${speler.id}.png`;
+
+        gameLadenGeluid.play();
+        spelerGekozenGeluid.play();
+
+        characterKiezen.style.display ='none';
+        spelLaden.style.display = 'block';
+
+        setTimeout(spelSpelen, 4000);
     })
 })
 
-function gameLaden(){
-    gameLadenGeluid.play();
-    spelerGekozenGeluid.play();
-    characterKiezen.style.display ='none';
-    spelLaden.style.display = 'block';
+// ------------------------speler kiezen--------------------------------------------------------------------------------------------------------------------
 
-    setTimeout(spelSpelen, 4000);
-}
-
-// ------------------------character kiezen---------------------------
-
-// ------------------------------spel spelen--------------------------
+// ------------------------------spel spelen-------------------------------------------------------------------------------------------------------------------
 
 const achtergrondMuziekje = new Audio('./audio/achtergrondmuziek.mp3') // https://pixabay.com/sound-effects/game-music-loop-6-144641/
 
@@ -154,9 +156,9 @@ function spelSpelen(){
     achtergrondAfbeelding.style.backgroundImage = 'url(./images/arcade.png)';
 }
 
-const klikOpKeuze = document.querySelector('.spelenpagina section p')
+// tekst klik op een afbeelding laten knipperen----------------------------------------------------------------------
 
-// tekst laten knipperen
+const klikOpKeuze = document.querySelector('.spelenpagina section p')
 
 setInterval( () => { 
     if (klikOpKeuze.style.visibility === 'hidden') { // chatGPT heeft me geholpen met de term visibility
@@ -166,7 +168,7 @@ setInterval( () => {
     }
 }, 1500)
 
-// klikken op keuze steen papier schaar, werkelijk spelen
+// klikken op keuze steen papier schaar, werkelijk spelen ------------------------------------------------------------
 
 const afbeeldingSteen = document.getElementById('steen')
 const afbeeldingPapier = document.getElementById('papier')
@@ -175,87 +177,83 @@ const computerAfbeelding = document.getElementById('computerkeuze')
 const resultaat = document.querySelector('header div h2')
 const afbeeldingGebruikerkeuze = document.querySelector('.gebruikerkeuze')
 const audioPartyHorn = new Audio('./audio/partyhorn.mp3') // bron https://pixabay.com/sound-effects/party-horn-68443/
+let randomKeuze = 0
 
 afbeeldingGebruikerkeuze.style.display = 'none'
 computerAfbeelding.style.display = 'none'
 
-const afbeeldingen = {
+const afbeeldingen = { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
     steen: "./images/rock.png",
     papier: "./images/papier.jpeg",
     schaar: "./images/schaar.png"
 }
 
-afbeeldingSteen.addEventListener('click', () => {
-    spelen('steen');
-    afbeeldingGebruikerkeuze.src = afbeeldingen.steen;
-})
+const afbeeldingenOpties = [afbeeldingSteen, afbeeldingPapier, afbeeldingSchaar];
 
-afbeeldingPapier.addEventListener('click', () => {
-    spelen('papier');
-    afbeeldingGebruikerkeuze.src = afbeeldingen.papier;
-})
-
-afbeeldingSchaar.addEventListener('click', () => {
-    spelen('schaar');
-    afbeeldingGebruikerkeuze.src = afbeeldingen.schaar;
+afbeeldingenOpties.forEach(afbeeldingOptie => {
+    afbeeldingOptie.addEventListener('click', () => {
+        const keuze = afbeeldingOptie.id;
+        spelen(keuze);
+        afbeeldingGebruikerkeuze.src = afbeeldingen[keuze];
+    })
 })
 
 function getRandomKeuze(){ 
     const keuzes = ["steen", "papier", "schaar"];
     const randomIndex = Math.floor(Math.random() * 3);
-    return keuzes[randomIndex]; // hier heeft ChatGPT me bij geholpen
+    randomKeuze = keuzes[randomIndex];
 }
 
-function spelen(gebruikerKeuze) {
+function spelen(gebruikerKeuze){
     afbeeldingGebruikerkeuze.style.display = 'block';
     computerAfbeelding.style.display = 'block'
-    computerAfbeelding.src = "./images/loading.gif"; // loading gif bron https://www.google.com/search?q=loading+gif+png&tbm=isch&ved=2ahUKEwiR3KDg-4SFAxXUj_0HHQfyC-oQ2-cCegQIABAA&oq=loading+gif+png&gs_lp=EgNpbWciD2xvYWRpbmcgZ2lmIHBuZzIFEAAYgAQyBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB4yBhAAGAUYHjIGEAAYBRgeMgYQABgFGB4yBhAAGAUYHjIGEAAYBRgeSN0ZUOMCWJcWcAZ4AJABAJgBOaAB5QKqAQE3uAEDyAEA-AEBigILZ3dzLXdpei1pbWfCAgoQABiABBiKBRhDwgIGEAAYBxgewgIHEAAYgAQYE8ICCBAAGAUYHhgTwgIIEAAYCBgeGBOIBgE&sclient=img&ei=v_P7ZdGADNSf9u8Ph-Sv0A4&bih=732&biw=1512&prmd=ivnbz&rlz=1C5MACD_enNL1064NL1064#imgrc=9rvBEbPXvUM4PM
+    computerAfbeelding.src = './images/loading.gif'; // loading gif bron https://www.google.com/search?q=loading+gif+png&tbm=isch&ved=2ahUKEwiR3KDg-4SFAxXUj_0HHQfyC-oQ2-cCegQIABAA&oq=loading+gif+png&gs_lp=EgNpbWciD2xvYWRpbmcgZ2lmIHBuZzIFEAAYgAQyBBAAGB4yBBAAGB4yBBAAGB4yBBAAGB4yBhAAGAUYHjIGEAAYBRgeMgYQABgFGB4yBhAAGAUYHjIGEAAYBRgeSN0ZUOMCWJcWcAZ4AJABAJgBOaAB5QKqAQE3uAEDyAEA-AEBigILZ3dzLXdpei1pbWfCAgoQABiABBiKBRhDwgIGEAAYBxgewgIHEAAYgAQYE8ICCBAAGAUYHhgTwgIIEAAYCBgeGBOIBgE&sclient=img&ei=v_P7ZdGADNSf9u8Ph-Sv0A4&bih=732&biw=1512&prmd=ivnbz&rlz=1C5MACD_enNL1064NL1064#imgrc=9rvBEbPXvUM4PM
 
     setTimeout(() => {
-        const computerKeuze = getRandomKeuze();
-        computerAfbeelding.src = afbeeldingen[computerKeuze];
-        bepaalResultaat(gebruikerKeuze, computerKeuze);
+        getRandomKeuze();
+        computerAfbeelding.src = afbeeldingen[randomKeuze];
+        bepaalResultaat(gebruikerKeuze, randomKeuze);
     }, 600);
 }
 
-function bepaalResultaat(gebruikerKeuze, computerKeuze) {
-    if (gebruikerKeuze === computerKeuze) {
+function bepaalResultaat(gebruikerKeuze, randomKeuze) {
+    if (gebruikerKeuze === randomKeuze) {
         resultaat.textContent = 'Gelijkspel!';
-    } else if ((gebruikerKeuze === "steen" && computerKeuze === "schaar") || (gebruikerKeuze === 'papier' && computerKeuze === 'steen') || (gebruikerKeuze === 'schaar' && computerKeuze === "papier")) {
-        // confetti
+    } else if ((gebruikerKeuze === 'steen' && randomKeuze === 'schaar') || (gebruikerKeuze === 'papier' && randomKeuze === 'steen') || (gebruikerKeuze === 'schaar' && randomKeuze === 'papier')) {
+       
+        // confetti & partyhorn
         setTimeout(() => {
-            confetti.style.display = "none";
+            confetti.style.display = 'none';
         },1500);
-        confetti.style.display = "block";
+        confetti.style.display = 'block';
         audioPartyHorn.play();
     
-        resultaat.textContent = "Gefeliciteerd, je hebt gewonnen!";
+        resultaat.textContent = 'Gefeliciteerd, je hebt gewonnen!';
         scoreBordToenemen()
-
     } else {
         const audioError = new Audio('./audio/error.mp3') // bron error geluid https://pixabay.com/sound-effects/error-126627/
         audioError.play();
 
-        resultaat.textContent = "Jammer, je hebt verloren :(";
+        resultaat.textContent = 'Jammer, je hebt verloren :(';
         scoreBordAfnemen()
     }
 }
 
-// scoreboard
+// scoreboard-----------------------------------------------------------
 
 const scoreTekst = document.querySelector('header div p')
 let score = 0
 
 function scoreBordToenemen(){
-    scoreTekst.style.display = "block";
+    scoreTekst.style.display = 'block';
     score = score + 1;
-    scoreTekst.textContent = `Jouw score: + ${score}`; 
+    scoreTekst.textContent = `Jouw score: ${score}`; 
 }
 
 function scoreBordAfnemen(){
-    scoreTekst.style.display = "block";
+    scoreTekst.style.display = 'block';
     score = score - 1;
     scoreTekst.textContent = `Jouw score: ${score}`; 
 }
 
-// -----------------------------------spel spelen---------------------------------------
+// -----------------------------------spel spelen--------------------------------------------------------------------------------------------------------------------------------
